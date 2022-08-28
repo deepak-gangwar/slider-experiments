e75c98c3ebb175f93a5db8fa50e2b933ddbfc27e
Deepak <deepakgangwar4265@gmail.com>
2022-08-28 12:27:42 +0530

    Add cool console message for devs

--- app/index.js
+++ app/index.js
@@ -3,10 +3,38 @@ import { Slider } from "./components/slider"
 class App {
     constructor() {
         this.init()
+        this.styleConsoleForDevs()
     }
 
     init() {
         new Slider()
+    }
+
+    styleConsoleForDevs() {
+        // 1. Pass CSS styles in an array
+        const style1 = [
+            'font-family: Helvetica',
+            'padding-top: 20px',
+            'padding-bottom: 20px',
+            'font-size: 1.2rem',
+        ].join(';')// 2. Concatenate the individual array item and concatenate them into a string separated by a semi-colon (;)
+    
+        const style2 = [
+            'color: red',
+            'padding-top: 20px',
+            'padding-bottom: 20px',
+            'font-size: 1.2rem',
+        ].join(';')
+    
+        const styleLink = [
+            'font-size: .7rem',
+            'padding-bottom: 20px',
+            // 'font-family: Helvetica',
+        ].join(';')
+    
+        // 3. Pass the styles variable
+        console.log('%cMade with %c♥%c by Deepak Gangwar%c\n✌ https://deepakgangwar.me', style1, style2, style1, styleLink)
+        // console.log('http://deepakgangwar.me');
     }
 }
 new App()
